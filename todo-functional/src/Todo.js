95ac8821f9c2519210ad2a49e3cb99b5e075a08a
Reuben T Allik <reubenallik@gmail.com>
2021-03-17 21:50:29 -0400

    branch test

--- todo-functional/src/Todo.js
+++ todo-functional/src/Todo.js
@@ -1,23 +1,21 @@
-//For refactoring
+import React, {useState} from 'react';
 
-// import React, {useState} from 'react';
+const Todo = (props) => {
+    const { id, todo, done } = props;
+    // const [ complete, setComplete ] = useState({})
+    const doneBtn = () => {
+        // setComplete((old) => {
+        //     return !old;
+        // })
+        console.log("heheheh")
+    }
+    return (
+        <article className='card' id={id}>
+            <div className='done'></div>
+            <h4 className='txt'>{todo}</h4>
+            <button className='btn' onClick={doneBtn}>Done</button>
+        </article>
+    );
+}
 
-// const Todo = (props) => {
-//     const { id, todo, done } = props;
-//     // const [ complete, setComplete ] = useState({})
-//     const doneBtn = () => {
-//         // setComplete((old) => {
-//         //     return !old;
-//         // })
-//         console.log("heheheh")
-//     }
-//     return (
-//         <article className='card' id={id}>
-//             <div className='done'></div>
-//             <h4 className='txt'>{todo}</h4>
-//             <button className='btn' onClick={doneBtn}>Done</button>
-//         </article>
-//     );
-// }
-
-// export default Todo;
+export default Todo;
