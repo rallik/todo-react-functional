45e413f23e9226bb95c51dc1ab509ee06cafd309
Reuben T Allik <reubenallik@gmail.com>
2021-03-21 22:25:27 -0400

    added chr count on input

--- todo-functional/src/Inputs.js
+++ todo-functional/src/Inputs.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Inputs = () => {
+    const [chrCount, setChrCount] = useState(' ');
+
+
     return (
-        <React.Fragment>
-            <input type="text" name='todo'/>
-            <input type="submit" value="Add"/>
-        </React.Fragment>
+        <form className='newTodoForm'>
+            <input type="text" name='todo' className='newTodo' onChange={(e) => setChrCount(e.target.value.length)}/>
+            <label htmlFor="todo" className='chr'>{chrCount}</label>
+            <input type="submit" value="Add" className='submitBtn'/>
+        </form>
     )
 }
 
